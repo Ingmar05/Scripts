fe3fe37a76b4f58c939c475e00fc2b465e22644c
Ingmar Sell <ingmarsell@pop-os.localdomain>
2019-05-14 20:04:19 -0400

    Add digidoc4 installer

---
+++ bash/digidoc4/install-open-eid-disco.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Ubuntu 18.04 is not officially supported, use at you own risk!"
+echo "Downloading libzip4"
+wget http://http.us.debian.org/debian/pool/main/libz/libzip/libzip4_1.5.1-4_amd64.deb
+echo "Installing libzip4"
+sudo dpkg -i libzip4_1.5.1-4_amd64.deb
+echo "Downloading official installer"
+wget https://installer.id.ee/media/install-scripts/install-open-eid.sh
+echo "Setting permissions"
+sudo chmod 775 install-open-eid.sh
+echo "Modifying official installer"
+sed -i -e 's/$(lsb_release -cs)/bionic/g' install-open-eid.sh
+echo "Running official installer"
+./install-open-eid.sh
+echo "Cleaning up"
+rm install-open-eid.sh
+rm libzip4_1.5.1-4_amd64.deb
+echo "Done!"
